0fe9cc3970101bf9bbdddbae846a4abd74e26ba3
Ecomm <ashwinisaxena4@gmail.com>
2022-09-17 20:28:48 +0530

    First Complete Commit

--- ec/blog/urls.py
+++ ec/blog/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.index, name="ShopHome"),
+    path("", views.index, name="blogHome"),
+    path("blogpost/<int:id>", views.blogpost, name="blogHome"),
 
 ]
--- ec/shop/views.py
+++ ec/shop/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Product, Contact, Orders, OrderUpdate
 from math import ceil
 import json
-from django.views.decorators.csrf import csrf_exempt
+
 # Create your views here.
 from django.http import HttpResponse
 
@@ -19,12 +19,35 @@ def index(request):
     params = {'allProds':allProds}
     return render(request, 'shop/index.html', params)
 
+def searchMatch(query, item):
+    '''return true only if query matches the item'''
+    if query in item.desc or query in item.product_name or query in item.category:
+        return True
+    else:
+        return False
 
+def search(request):
+    query= request.GET.get('search')
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prodtemp = Product.objects.filter(category=cat)
+        prod=[item for item in prodtemp if searchMatch(query, item)]
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        if len(prod)!= 0:
+            allProds.append([prod, range(1, nSlides), nSlides])
+    params = {'allProds': allProds, "msg":""}
+    if len(allProds)==0 or len(query)<4:
+        params={'msg':"Please make sure to enter relevant search query"}
+    return render(request, 'shop/search.html', params)
 def about(request):
     return render(request, 'shop/about.html')
 
 
 def contact(request):
+    thank = False
     if request.method=="POST":
         name = request.POST.get('name', '')
         email = request.POST.get('email', '')
@@ -32,7 +55,8 @@ def contact(request):
         desc = request.POST.get('desc', '')
         contact = Contact(name=name, email=email, phone=phone, desc=desc)
         contact.save()
-    return render(request, 'shop/contact.html')
+        thank = True
+    return render(request, 'shop/contact.html',  {'thank':thank})
 
 
 def tracker(request):
@@ -56,8 +80,7 @@ def tracker(request):
     return render(request, 'shop/tracker.html')
 
 
-def search(request):
-    return render(request, 'shop/search.html')
+
 
 
 def productv(request, myid):
@@ -79,15 +102,11 @@ def checkout(request):
         zip_code = request.POST.get('zip_code', '')
         phone = request.POST.get('phone', '')
         order = Orders(items_json=items_json, name=name, email=email, address=address, city=city,
-                       state=state, zip_code=zip_code, phone=phone, amount=amount)
+                       state=state, zip_code=zip_code, phone=phone,amount=amount)
         order.save()
         update = OrderUpdate(order_id=order.order_id, update_desc="The order has been placed")
         update.save()
         thank = True
         id = order.order_id
-       # return render(request, 'shop/checkout.html', {'thank':thank, 'id': id})
-
+        return render(request, 'shop/checkout.html', {'thank':thank, 'id': id})
     return render(request, 'shop/checkout.html')
-@csrf_exempt
-def handlerequest(request):
-    pass
